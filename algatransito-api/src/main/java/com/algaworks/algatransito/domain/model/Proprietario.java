ea3a482899f1571108867fb7f07d6842a1bb68c2
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-10-27 20:20:10 -0400

    feat: Add project Lombok dependency and enable @Getter and @Setter in model class.

--- algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,42 +1,20 @@
 package com.algaworks.algatransito.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+* Anotações do Lombok ajudam em tempo de desenvolvimento a reduzir
+* código repetitivo padrão (boilerplate).
+* No caso está habilitando getters e setters para os atributos da classe.
+*/
+
+@Getter
+@Setter
 public class Proprietario {
 
     private Long id;
     private String nome;
     private String email;
     private String telefone;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
 }
