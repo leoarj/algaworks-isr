767aecc21526016325277b7382830a547a32aa62
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-10-28 21:37:22 -0400

    Implements mapping of Proprietario class to entity on database.

--- algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,5 +1,7 @@
 package com.algaworks.algatransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,10 +13,24 @@ import lombok.Setter;
 
 @Getter
 @Setter
+// Implementa equals() e hashCode() para apenas atributos explícitamente marcados.
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+// Diz que a classe é um mapeamento para uma entidade.
+@Entity
+//@Table(name = "tb_proprietario") // Caso o nome da tabela no db fosse diferente.
 public class Proprietario {
 
+    // Inclui id para as lógicas de equals() e hashCode().
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
+
+    // Mapeia propriedade "telefone" para a coluna "fone", @Column utilizada
+    // justamente porque o nome difere no mapeamento.
+    @Column(name = "fone")
     private String telefone;
 }
