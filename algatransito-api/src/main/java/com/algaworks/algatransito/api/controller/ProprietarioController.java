f158926f71ab11c1ff15dfd906bbb9c7efe2e688
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-10-30 22:07:16 -0400

    feat: Add endpoint for search of singleton resource Proprietario.

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -7,7 +7,10 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -15,6 +18,8 @@ import java.util.List;
 
 @AllArgsConstructor // Para criar construtor com todos os argumemtos.
 @RestController
+// Para mapear a rota padrão dos endpoints e não ter que repetir em cada um.
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
     /*
@@ -40,11 +45,27 @@ public class ProprietarioController {
 //        this.proprietarioRepository = proprietarioRepository;
 //    }
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> listar() {
         //return proprietarioRepository.findAll();
         //return proprietarioRepository.findByNome("Proprietário 2");
         return proprietarioRepository.findByNomeContaining("3");
+    }
+
+    /*
+    * Método para implementar endpoint de busca de recurso único.
+    * Verbo GET com variável de caminho {proprietarioId}
+    * que é vinculada ao argumento do método via anotação @PathVariable.
+    *
+    * Retornando um ResponseEntity para melhor manipulação do retorno,
+    * como o código de status do HTTP.
+    */
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
