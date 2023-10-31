46fab604c6955fc2ee57e205c1c92eace66d7797
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-10-30 21:05:35 -0400

    feat: Add injection and usage of repository for Proprietario entity in controller.

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,15 +1,19 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor // Para criar construtor com todos os argumemtos.
 @RestController
 public class ProprietarioController {
 
@@ -21,28 +25,24 @@ public class ProprietarioController {
     */
 
     /*
-    * EntityManager é uma interface que é implementada pelo motor de acesso ao banco de dados (Hibernate)
-    * e fornece acesso e manipulação dos objetos do banco de dados.
+    * Injetando repository que estende JpaRepository,
+    * para encapsular o acesso a dados e disponibilidade de métodos padrão.
     *
-    * A anotação @PersistenceContext diz ao Spring para injetar uma instânica de um EntityManager para uso.
+    * Injetar com @Autowired = possível
+    *
+    * Injetar via construtor = Possíve e mais recomendado (boa prática).
     */
-    @PersistenceContext
-    private EntityManager manager;
+
+    //@Autowired
+    private ProprietarioRepository proprietarioRepository;
+
+//    public ProprietarioController(ProprietarioRepository proprietarioRepository) {
+//        this.proprietarioRepository = proprietarioRepository;
+//    }
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-
-        /*
-        * Através do EntityManager é possível criar uma consulta tipada
-        * e a partir dela obter objetos mapeados a partir banco.
-        */
-
-        return manager
-                .createQuery("from Proprietario", Proprietario.class)
-                .getResultList();
-
-//        TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
-//        return query.getResultList();
+        return proprietarioRepository.findAll();
     }
 
 }
