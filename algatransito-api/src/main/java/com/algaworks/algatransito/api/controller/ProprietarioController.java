7269fcceae15e226286b8fbb158a33861fed4b87
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-03 21:35:33 -0400

    feat: Add exception handler for NegocioException in controller of Proprietario.

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.api.controller;
 
+import com.algaworks.algatransito.domain.exception.NegocioException;
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import com.algaworks.algatransito.domain.service.RegistroProprietarioService;
@@ -149,6 +150,26 @@ public class ProprietarioController {
         //proprietarioRepository.deleteById(proprietarioId);
         registroProprietarioService.excluir(proprietarioId);
         return ResponseEntity.noContent().build();
+    }
+
+    /*
+    * Método para capturar exceções específicas de negócio,
+    * a fim de poder retornar um código de status apropriado,
+    * juntamente com mensagem de erro reduzida.
+    *
+    * Em resumo, especificar que o erro é do consumidor da API
+    * e não do backend.
+    *
+    * @ExceptionHandler(Classe.class) = Indica que o método
+    * é um capturador de erros no controlador de uma determinada exception.
+    *
+    * Aqui além de fornecer um status apropriado, também seria possível
+    * registrar um log, notificações entre outras ações.
+    */
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<String> capturar(NegocioException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 }
