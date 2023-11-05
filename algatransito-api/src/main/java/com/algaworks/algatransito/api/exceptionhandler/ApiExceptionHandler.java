9cf59a319f9d5f1fcb2afb4da32e9b73537048bb
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-05 15:30:35 -0400

    feat: Add custom implementation of problem detail for MethodArgumentNotValidException.

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.algaworks.algatransito.api.exceptionhandler;
 
 import com.algaworks.algatransito.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
 
 /*
 * Ponto único para tratamento de exceções de forma global.
@@ -16,6 +23,25 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /*
+    * Método para personalizar a resposta de acordo com a RFC 7807,
+    * para a exceção de MethodArgumentNotValidException.
+    */
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://algatransito.com/erros/campos-invalidos"));
+
+        // Deve repassar uma instância de ProblemDetail (do pacote org.springframework.http)
+        // para que a alteração tenha efeito.
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     /*
      * Método para capturar exceções específicas de negócio,
