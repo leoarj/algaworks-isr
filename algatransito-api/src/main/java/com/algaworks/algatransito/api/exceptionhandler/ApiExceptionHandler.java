3c6c14b2d23c4ccc60d0c2927b45b44997314380
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-05 17:48:35 -0400

    refactor: Change handler of NegocioException to return ProblemDetail object.

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -83,7 +80,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
      */
 
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<String> capturar(NegocioException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ProblemDetail handleNegocio(NegocioException e) {
+        //return ResponseEntity.badRequest().body(e.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST); //400
+        problemDetail.setTitle(e.getMessage());
+        problemDetail.setType(URI.create("https://algatransito.com/erros/regra-de-negocio"));
+
+        return problemDetail;
     }
 }
