a30d4ad06a3a618e0e59e64894115b090c3ff2ca
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-06 21:59:12 -0400

    feat: Add representation model of Veiculo resource.

---
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/model/VeiculoModel.java
@@ -0,0 +1,24 @@
+package com.algaworks.algatransito.api.model;
+
+import com.algaworks.algatransito.domain.model.StatusVeiculo;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+/*
+* Classe para separação entre o Modelo de Domínio e o Modelo de Representação do recurso de Veiculo.
+*/
+
+@Getter
+@Setter
+public class VeiculoModel {
+    private Long id;
+    private String nomeProprietario;
+    private String marca;
+    private String modelo;
+    private String numeroPlaca;
+    private StatusVeiculo status;
+    private OffsetDateTime dataCadastro;
+    private OffsetDateTime dataApreensao;
+}
