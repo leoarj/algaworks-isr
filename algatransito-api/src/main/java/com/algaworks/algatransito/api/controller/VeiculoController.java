98fd7e3abf5ef43dc22c11a1d8abe420d08bae76
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-06 22:32:36 -0400

    feat: Add model mapper bean in controller of Veiculo resource.

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -7,6 +7,7 @@ import com.algaworks.algatransito.domain.repository.VeiculoRepository;
 import com.algaworks.algatransito.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ public class VeiculoController {
 
     private final VeiculoRepository veiculoRepository;
     private final RegistroVeiculoService registroVeiculoService;
+    private final ModelMapper modelMapper;
 
     @GetMapping
     public List<Veiculo> listar() {
@@ -29,18 +31,7 @@ public class VeiculoController {
     @GetMapping("/{veiculoId}")
     public ResponseEntity<VeiculoModel> buscar(@PathVariable Long veiculoId) {
         return veiculoRepository.findById(veiculoId)
-                .map(veiculo -> {
-                    var veiculoModel = new VeiculoModel();
-                    veiculoModel.setId(veiculo.getId());
-                    veiculoModel.setNomeProprietario(veiculo.getProprietario().getNome());
-                    veiculoModel.setMarca(veiculo.getMarca());
-                    veiculoModel.setModelo(veiculo.getModelo());
-                    veiculoModel.setNumeroPlaca(veiculo.getPlaca());
-                    veiculoModel.setStatus(veiculo.getStatus());
-                    veiculoModel.setDataCadastro(veiculo.getDataCadastro());
-                    veiculoModel.setDataApreensao(veiculo.getDataApreensao());
-                    return veiculoModel;
-                })
+                .map(veiculo -> modelMapper.map(veiculo, VeiculoModel.class))
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
