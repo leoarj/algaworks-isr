eaf64ea0c9a2f1e84a503c395d56333e04a973a7
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-12 18:34:34 -0400

    feat: Add endpoint non-CRUD operations (applying to business rules for apprehension flow).

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -6,6 +6,7 @@ import com.algaworks.algatransito.api.model.input.VeiculoInput;
 import com.algaworks.algatransito.domain.exception.NegocioException;
 import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.repository.VeiculoRepository;
+import com.algaworks.algatransito.domain.service.ApreensaoVeiculoService;
 import com.algaworks.algatransito.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -22,6 +23,7 @@ public class VeiculoController {
 
     private final VeiculoRepository veiculoRepository;
     private final RegistroVeiculoService registroVeiculoService;
+    private final ApreensaoVeiculoService apreensaoVeiculoService;
     private final VeiculoAssembler veiculoAssembler;
 
     @GetMapping
@@ -45,5 +47,30 @@ public class VeiculoController {
 
         return veiculoAssembler.toModel(veiculoCadastrado);
         //return veiculoAssembler.toModel(registroVeiculoService.cadastrar(veiculo));
+    }
+
+    /*
+     * Endpoint para fluxo de apreensão de um veículo.
+     * Foi escolhido o verbo PUT pois o mesmo é indempotente
+     * e também porque devemos conhecer a URI do recurso (Veiculo)
+     * para se aplicar a apreensão.
+     */
+
+    @PutMapping("/{veiculoId}/apreensao")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void apreender(@PathVariable Long veiculoId) {
+        apreensaoVeiculoService.apreender(veiculoId);
+    }
+
+    /*
+     * Endpoint para fluxo de remoção de apreensão de um veículo.
+     * Foi escolhido o verbo DETELE pois é a semântica ideal
+     * para "remoção" de um recurso, mesmo que não seja CRUD.
+     */
+    
+    @DeleteMapping("/{veiculoId}/apreensao")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerApreensao(@PathVariable Long veiculoId) {
+        apreensaoVeiculoService.removerApreensao(veiculoId);
     }
 }
