a89d1013c97740ec1061a806892e865a4901387c
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-13 22:24:16 -0400

    feat: Add generic base assembler for code reuse.

---
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/assembler/BaseAssembler.java
@@ -0,0 +1,62 @@
+package com.algaworks.algatransito.api.assembler;
+
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Classe base para assemblers.
+ * <br/><br/>
+ * Estender essa classe para reaproveitar métodos de mapeamento dos objetos de
+ * entidade, modelo de representação de entrada e modelo de representaçao de saída.
+ *
+ * @param <T> Tipo parametrizado para a classe da Entidade.
+ * @param <U> Tipo parametrizado para a classe do modelo de representação de input de dados.
+ * @param <V> Tipo parametrizado para a classe do modelo de representação de output de dados.
+ */
+//@AllArgsConstructor
+@Component
+public abstract class BaseAssembler<T, U, V> {
+
+    private final ModelMapper modelMapper;
+
+    public BaseAssembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public T toEntity(U inputModel) {
+        return modelMapper.map(inputModel, getClassOfEntity());
+    }
+
+    public V toModel(T entity) {
+        return modelMapper.map(entity, getClassOfOutputModel());
+    }
+
+    public List<V> toCollectionModel(List<T> entities) {
+        return entities.stream()
+                .map(this::toModel)
+                .toList();
+    }
+
+    /*
+    * Para forçar o fornecimento da class de cada tipo no assembler concreto.
+    */
+
+    /**
+     * Método para obter a class do tipo da entidade.<br/>
+     * Utilizado no método toEntity(U inputModel).
+     *
+     * @return Class do tipo da entidade.
+     */
+    protected abstract Class<T> getClassOfEntity();
+
+    /**
+     * Método para obter a class do tipo do modelo de representação de saida.<br/>
+     * Utilizado no método toModel(T entity).
+     *
+     * @return Class do tipo do modelo de representação de saída.
+     */
+    protected abstract Class<V> getClassOfOutputModel();
+}
