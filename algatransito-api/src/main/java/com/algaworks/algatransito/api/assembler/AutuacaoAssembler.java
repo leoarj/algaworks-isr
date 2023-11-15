f6f9aae55a9ce0e8167822d9bd1216923c1b5b9e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-14 20:52:16 -0400

    chore: Remove annotations and optimize imports.

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/assembler/AutuacaoAssembler.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/assembler/AutuacaoAssembler.java
@@ -1,15 +1,12 @@
 package com.algaworks.algatransito.api.assembler;
 
+import com.algaworks.algatransito.api.assembler.base.BaseAssembler;
 import com.algaworks.algatransito.api.model.AutuacaoModel;
 import com.algaworks.algatransito.api.model.input.AutuacaoInput;
 import com.algaworks.algatransito.domain.model.Autuacao;
-import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-//@AllArgsConstructor
 @Component
 public class AutuacaoAssembler extends BaseAssembler<Autuacao, AutuacaoInput, AutuacaoModel> {
 
@@ -21,7 +18,5 @@ public class AutuacaoAssembler extends BaseAssembler<Autuacao, AutuacaoInput, Au
     protected Class<Autuacao> getClassOfEntity() { return Autuacao.class; }
 
     @Override
-    protected Class<AutuacaoModel> getClassOfOutputModel() {
-        return AutuacaoModel.class;
-    }
+    protected Class<AutuacaoModel> getClassOfOutputModel() { return AutuacaoModel.class; }
 }
